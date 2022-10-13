ba004d62a1b40f20d6fd6a0ad159634a34f24c3f
Nuno Mestre <nuno_mestre@student.uml.edu>
2022-10-12 22:48:17 -0400

    update

---
+++ easy/14_Longest_Common_Prefix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& s) {
+        int ans = s[0].length();
+        for(int i=1; i< s.size(); ++i){
+            int j = 0;
+            while(j < s[i].length() && s[i][j]==s[0][j]) {
+                j++;
+            }
+            ans = min(ans, j);
+        }
+        return s[0].substr(0, ans);
+    }
+};
